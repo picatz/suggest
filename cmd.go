db07c0352b3c0c676ae8543b45dbb68d96e598d0
Kent 'picat' Gruber <kgruber1@emich.edu>
2018-11-15 13:37:30 -0500

    add initial cmd implementation

---
+++ cmd.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"strings"
+
+	"github.com/picatz/suggest"
+)
+
+func init() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			os.Exit(0)
+		}
+	}()
+}
+
+var (
+	StatusNoArgs        = 1
+	StatusGetErr        = 2
+	StatusNoSuggestions = 3
+)
+
+func main() {
+
+	args := os.Args[1:]
+
+	if len(args) <= 0 {
+		fmt.Println("no arguments given to get suggestions for")
+		os.Exit(StatusNoArgs)
+	}
+
+	suggestions, err := suggest.Get(strings.Join(args, " "))
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(StatusGetErr)
+	}
+
+	if len(suggestions) == 0 {
+		os.Exit(StatusNoSuggestions)
+	}
+
+	for _, suggestion := range suggestions {
+		fmt.Println(suggestion)
+	}
+}
